8ee47029282b915153d34760e0d38403852a7faf
Loai Qubti <loai.qubti@gmail.com>
2020-05-25 00:28:29 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,52 @@
+
+# You're an engineer @meet, you're building a printer that prints paper has a header title saying "Welcome to MEET Summer", along with the year.
+# MEET's CEO doesn't want to get a new printer every year, so he has asked you to write a function with the option to update the year.
+
+# Write a function called "meets_printer("
+
+def meets_printer(year):
+  print("#######################################")
+  print("#######################################")
+  print("########    WELCOME TO MEET    ########")
+  print("########      Summer "+str(year)+"      ########")
+  print("#######################################")
+  print("#######################################")
+  print("#######################################")
+  print("#######################################")
+  print("#######################################")
+  print("#######################################")
+  print("#######################################")
+  print("#######################################")
+  print("#######################################")
+  print("#######################################")
+  print("#######################################")
+  print("#######################################")
+  print("#######################################")
+  print("#######################################")
+
+meets_printer(2006)
+
+
+print(("aH".swapcase() + "MAD".swapcase()))
+
+city_info = {"new_york" : { "mayor" : "Bill DeBlasio",
+							"population" : 8337000,
+							"website" : "http://www.nyc.gov"
+							},
+			 "los_angeles" : { "mayor" : "Eric Garcetti",
+							   "population" : 3884307,
+							   "website" : "http://www.lacity.org"
+							},
+			 "miami" : { "mayor" : "Tomás Regalado",
+					     "population" : 419777,
+						 "website" : "http://www.miamigov.com"
+					    },
+			 "chicago" : { "mayor" : "Rahm Emanuel",
+						   "population" : 2695598,
+						   "website" : "http://www.cityofchicago.org/"
+						}
+		    }
+print(city_info["chicago"]["mayor"])
+print(len(city_info)) 
+hi = "bye"
+print(dict1)
